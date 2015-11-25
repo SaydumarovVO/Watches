376ea95c05daf294ad3131bb2f603c15c9a18042
SaydumarovVO <saidumarov@phystech.edu>
2015-11-26 00:22:31 +0300

    working release of Watches. 1 problem still exists

--- src/watches/client/Watches.java
+++ src/watches/client/Watches.java
@@ -7,58 +7,25 @@ import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.DOM;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.ClickEvent;
+
+//import java.text.SimpleDateFormat;
 import java.util.*;
 
-/**
- * Entry point classes define <code>onModuleLoad()</code>
- */
 public class Watches implements EntryPoint {
 
-    /**
-     * This is the entry point method.
-     */
     public void onModuleLoad() {
-        final Button button = new Button("Время");
         final Label label = new Label();
 
-        button.addClickHandler(new ClickHandler() {
-            public void onClick(ClickEvent event) {
-                if (label.getText().equals("")) {
-                    Timer t = new Timer(){
-                        public void run(){
-                            Date d = new Date();
-                            label.setText(String.valueOf(d));
-                        }
-                    };
-                    t.scheduleRepeating(1000);
-                } else {
-                    label.setText("");
+        if (label.getText().equals("")) {
+            Timer t = new Timer() {
+                public void run() {
+                    Date d = new Date();
+                    //SimpleDateFormat format1 = new SimpleDateFormat("hh:mm:ss");
+                    label.setText(String.valueOf(d));
                 }
-            }
-        });
-
-        // Assume that the host HTML has elements defined whose
-        // IDs are "slot1", "slot2".  In a real app, you probably would not want
-        // to hard-code IDs.  Instead, you could, for example, search for all
-        // elements with a particular CSS class and replace them with widgets.
-        //
-        RootPanel.get("slot1").add(button);
-        RootPanel.get("slot2").add(label);
-    }
-
-    private static class MyAsyncCallback implements AsyncCallback<String> {
-        private Label label;
-
-        public MyAsyncCallback(Label label) {
-            this.label = label;
+            };
+            t.scheduleRepeating(100);
         }
-
-        public void onSuccess(String result) {
-            label.getElement().setInnerHTML(result);
-        }
-
-        public void onFailure(Throwable throwable) {
-            label.setText("Failed to receive answer from server!");
-        }
+        RootPanel.get("slot").add(label);
     }
 }
