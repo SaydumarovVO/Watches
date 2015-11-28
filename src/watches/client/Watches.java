b1534d9302daa78671b3ce97dc0ceda7c809a1b7
SaydumarovVO <saidumarov@phystech.edu>
2015-11-28 10:04:56 +0300

    working release of Watches 1 problem with timer exists

--- src/watches/client/Watches.java
+++ src/watches/client/Watches.java
@@ -7,6 +7,7 @@ import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.DOM;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.i18n.client.DateTimeFormat;
 
 //import java.text.SimpleDateFormat;
 import java.util.*;
@@ -14,18 +15,46 @@ import java.util.*;
 public class Watches implements EntryPoint {
 
     public void onModuleLoad() {
-        final Label label = new Label();
+        final Label label1 = new Label();
+        final Button timerButton = new Button("Секундомер");
+        final Label label2 = new Label();
 
-        if (label.getText().equals("")) {
-            Timer t = new Timer() {
-                public void run() {
-                    Date d = new Date();
-                    //SimpleDateFormat format1 = new SimpleDateFormat("hh:mm:ss");
-                    label.setText(String.valueOf(d));
+        Timer t = new Timer() {
+            public void run() {
+                Date d = new Date();
+                DateTimeFormat fmt = DateTimeFormat.getFormat("HH:mm:ss");
+                label1.setText(String.valueOf(fmt.format(d)));
+            }
+        };
+        t.scheduleRepeating(1000);
+
+        timerButton.addClickHandler(new ClickHandler() {
+            public void onClick(ClickEvent event) {
+                if (label2.getText().equals("")) {
+                    Timer t = new Timer() {
+                        final Date date = new Date();
+
+                        public void run() {
+                            Date d = new Date();
+                            DateTimeFormat fmt = DateTimeFormat.getFormat("HH:mm:ss:SS");
+                            long k = d.getTime() - date.getTime() - 3 * 1000 * 60 * 60;
+                            Date n = new Date();
+                            n.setTime(k);
+                            label2.setText(String.valueOf(fmt.format(n)));
+
+                        }
+                    };
+                    t.scheduleRepeating(10);
                 }
-            };
-            t.scheduleRepeating(100);
-        }
-        RootPanel.get("slot").add(label);
+                else{
+                    //label2.fireEvent(event);
+                    label2.setText("");
+                }
+            }
+        });
+
+        RootPanel.get("slot1").add(label1);
+        RootPanel.get("slot2").add(timerButton);
+        RootPanel.get("slot3").add(label2);
     }
 }
